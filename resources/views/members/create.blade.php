7044f4a28a98d8fb55b5cd458aec484d74cb3789
Valentin <vpujiula@cifpfbmoll.eu>
2022-02-07 19:57:43 +0100

    update create

--- resources/views/members/create.blade.php
+++ resources/views/members/create.blade.php
@@ -66,12 +66,6 @@
 
             </div>
         </div>
-        <div class="col-xs-12 col-sm-12 col-md-12">
-            <div class="form-group">
-                <strong>Avatar</strong><br>
-                <input type="file" name="file" id="file">
-            </div>
-        </div>
         
         <div class="col-xs-12 col-sm-12 col-md-12 text-center">
                 <button type="submit" class="btn btn-warning">Submit</button>
